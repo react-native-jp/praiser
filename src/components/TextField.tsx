d7fad042c3671efa32142d4f68943d1ad87e76aa
YutamaKotaro <california.dreamin.never.die@gmail.com>
2019-11-02 23:08:02 +0900

    :zap: キーボード制御＆キーボードタイプを修正した

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { ViewStyle } from 'react-native'
+import { ViewStyle, Keyboard } from 'react-native'
 import { TextInput } from 'react-native-paper'
 import { COLOR } from '../constants'
 
 interface Props {
-  label: string;
-  value: string;
-  onChangeText: (str: string) => void;
-  style?: ViewStyle;
+  label: string
+  value: string
+  onChangeText: (str: string) => void
+  style?: ViewStyle
+  autoCompleteType?:
+    | 'off'
+    | 'username'
+    | 'password'
+    | 'email'
+    | 'name'
+    | 'tel'
+    | 'street-address'
+    | 'postal-code'
+    | 'cc-number'
+    | 'cc-csc'
+    | 'cc-exp'
+    | 'cc-exp-month'
+    | 'cc-exp-year'
+  secureTextEntry?: boolean
 }
 const theme = {
   dark: true,
@@ -20,12 +35,7 @@ const theme = {
 }
 
 function TextField(props: Props) {
-  const {
-    label,
-    value,
-    onChangeText = () => {},
-    style,
-  } = props
+  const { label, value, onChangeText = () => {}, style, autoCompleteType, secureTextEntry } = props
   return (
     <TextInput
       label={label}
@@ -34,8 +44,14 @@ function TextField(props: Props) {
       mode="outlined"
       theme={theme}
       style={style}
+      autoCompleteType={autoCompleteType}
+      autoCapitalize="none"
+      secureTextEntry={secureTextEntry}
     />
   )
 }
 
+export function dismiss() {
+  Keyboard.dismiss()
+}
 export default TextField
