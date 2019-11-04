0f4e9317f9c2d1e0ba019e39e71a92b5898f02be
YutamaKotaro <california.dreamin.never.die@gmail.com>
2019-11-05 02:05:03 +0900

    :sparkles: fix userInfo layout and behaivor

---
+++ src/components/pages/UserInfo/LabelValueContainer.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { COLOR } from '../../../constants'
+
+const styles = StyleSheet.create({
+  row: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    paddingHorizontal: 50,
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  labelContainer: {
+    minWidth: 100,
+  },
+  labelText: {
+    color: COLOR.WHITE,
+    fontSize: 18,
+  },
+  valueContainer: {
+    flexShrink: 1,
+    paddingLeft: 10,
+  },
+  valueText: {
+    color: COLOR.WHITE,
+    fontSize: 16,
+  },
+})
+
+interface Props {
+  label: string
+  value: string | number | null
+}
+
+function LabelViewContainer(props: Props) {
+  const { label, value = '' } = props
+
+  return (
+    <View style={styles.row}>
+      <View style={styles.labelContainer}>
+        <Text style={styles.labelText}>{label}</Text>
+      </View>
+      <View style={styles.valueContainer}>
+        <Text style={styles.valueText}>{value}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default LabelViewContainer
