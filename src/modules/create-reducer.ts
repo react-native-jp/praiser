02cf1af2bb5bef8860ee20eb8315213d37ed9c4d
Takagi Kensuke <janus.wel.3@gmail.com>
2019-12-02 13:40:44 +0900

    :wrench: ignore any

--- src/modules/create-reducer.ts
+++ src/modules/create-reducer.ts
@@ -1,13 +1,16 @@
 import * as Redux from 'redux'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AllActions<Type extends keyof any = string> = Record<Type, Redux.Action>
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Handler<State = any, Action extends Redux.Action = Redux.AnyAction> = (state: State, action: Action) => State
 
 type Handlers<State, Actions extends AllActions> = {
   [Type in keyof Actions]: Actions[Type] extends Redux.Action ? Handler<State, Actions[Type]> : never
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function createReducer<State, HandlerDefinitions extends Handlers<State, any>>(
   initialState: State,
   handlers: HandlerDefinitions,
