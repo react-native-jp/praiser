eb9ab5cb8b1a97edd995bd2c180baf0ad53c9c93
Takagi Kensuke <janus.wel.3@gmail.com>
2019-12-07 20:42:36 +0900

    :white_check_mark: write tests for usecases

--- src/usecases/todos.ts
+++ src/usecases/todos.ts
@@ -6,30 +6,30 @@ import { AppState } from '../modules'
 import { add, remove, toggle, update } from '../modules/todos'
 
 export function addAndSync(userId: string, newValues: Todo.Values) {
-  return function(dispatch: Dispatch) {
+  return async function(dispatch: Dispatch) {
     const newTodo = Todo.factory(newValues)
     dispatch(add(newTodo))
-    TodosRepository.add(userId, newTodo)
+    await TodosRepository.add(userId, newTodo)
   }
 }
 
 export function removeAndSync(userId: string, id: string) {
-  return function(dispatch: Dispatch) {
+  return async function(dispatch: Dispatch) {
     dispatch(remove(id))
     TodosRepository.remove(userId, id)
   }
 }
 
 export function toggleAndSync(userId: string, id: string) {
-  return function(dispatch: Dispatch, getState: () => AppState) {
+  return async function(dispatch: Dispatch, getState: () => AppState) {
     dispatch(toggle(id))
     const newValue = getState().todos[id].completedAt
     TodosRepository.toggle(userId, id, newValue)
   }
 }
 
-export function editTodo(userId: string, id: string, newValues: Todo.Values) {
-  return function(dispatch: Dispatch) {
+export function editAndSync(userId: string, id: string, newValues: Todo.Values) {
+  return async function(dispatch: Dispatch) {
     dispatch(update(id, newValues))
     TodosRepository.change(userId, id, newValues)
   }
